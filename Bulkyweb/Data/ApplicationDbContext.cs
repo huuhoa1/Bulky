29d532a86d57a92af3d9b82c43cfe7409c1f88e5
Your Name <you@example.com>
2025-08-07 16:27:41 -0500

    'categories'

--- Bulkyweb/Data/ApplicationDbContext.cs
+++ Bulkyweb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bulkyweb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bulkyweb.Data
 {
@@ -8,5 +9,6 @@ namespace Bulkyweb.Data
         {
             
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
