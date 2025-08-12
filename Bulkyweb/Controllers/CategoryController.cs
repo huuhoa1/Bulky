0ca6086d69bb39d6c4d1d603e785c16ba88e3856
Your Name <you@example.com>
2025-08-12 19:57:34 -0400

    Up to Custom Validations

---
+++ Bulkyweb/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Bulkyweb.Data;
+using Bulkyweb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkyweb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name1", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            return View();
+        }
+    }
+}
