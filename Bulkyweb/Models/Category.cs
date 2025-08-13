c6561d46f3b04a014be45d23512f86708ba37bdb
Your Name <you@example.com>
2025-08-13 12:57:23 -0400

    Added edit and delete functions

--- Bulkyweb/Models/Category.cs
+++ Bulkyweb/Models/Category.cs
@@ -8,11 +8,11 @@ namespace Bulkyweb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [MaxLength(30)]
+        [MaxLength(300)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
-        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "1 - Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
